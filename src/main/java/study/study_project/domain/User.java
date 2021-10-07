0fdb4e17a319236a0fdf5a45afe9fa7cef3cbcd5
hminkim <hminkim2@gmail.com>
2021-10-07 15:36:50 +0900

    계정 생성, 확인 화면 생성

---
+++ src/main/java/study/study_project/domain/User.java
@@ -0,0 +1,18 @@
+package study.study_project.domain;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private Long userNumber;
+
+    private String userName;
+    private String userId;
+    private String userPw;
+
+    public User(String userName, String userId, String userPw) {
+        this.userName = userName;
+        this.userId = userId;
+        this.userPw = userPw;
+    }
+}
